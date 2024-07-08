188a4472097c757fd7c4c9dd8ae003f8f2429e0e
fs <fahrulfajarsaputro@gmail.com>
2024-07-08 10:19:00 +0700

    test

--- src/components/Partner/Partner.jsx
+++ src/components/Partner/Partner.jsx
@@ -17,7 +17,7 @@ export default function Partner() {
     return (
         <div className="w-full">
             <div className="container w-full mt-20">
-                <h1 className="section-tittle-light text-center mt-5">Our Partner</h1>
+                <h1 className="section-tittle-light text-center mt-5">Our Partners</h1>
                 <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-10 mt-5">
                     {Partners.map((item) => (
                         <div key={item.id} className="w-full flex justify-center shadow-md items-center p-5 bg-gray-100 rounded-2xl cursor-pointer hover:bg-gray-300 duration-150">
